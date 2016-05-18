5cf79820c450edb2673df7a3681a620c926b8933
Teagan Glenn <teagan@Teagans-Air.hsd1.co.comcast.net>
2016-05-18 17:58:16 -0600

    Just serial connection

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-var config = require('./config.json');
-var GPIO = require('onoff').Gpio,
-    ph_up = new GPIO(config.valves.ph_up),
-    ph_down = new GPIO(config.valves.ph_down);
+// var config = require('./config.json');
+// var GPIO = require('onoff').Gpio,
+//     ph_up = new GPIO(config.valves.ph_up),
+//     ph_down = new GPIO(config.valves.ph_down);
 var serialport = require("serialport");
 var SerialPort = serialport.SerialPort;
 
-var serialPort = new SerialPort("/dev/" + config.ph_probe.device, {
-  baudrate: config.ph_probe.baud,
+var serialPort = new SerialPort("/dev/ttyAMA0", {
+  baudrate: 38400,
   parser: serialport.parsers.readline('\n');
 });
 
